156ffb65a3aa936965514aa5392872086eb7b875
Stephen Fang <safang99@gmail.com>
2019-10-29 19:34:57 -0400

    moved to local board

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import Chessboard from 'chessboardjsx'
+
+import LocalBoard from "./LocalBoard"
 
 export const App = (props) => {
-  const boardsContainer = {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    flexWrap: "wrap",
-    width: "100vw",
-    marginTop: 30,
-    marginBottom: 50
-  };
-
-
   return (
-    <div style={boardsContainer}>
-      <Chessboard position="start"/>
+    <div id="chessBoard">
+      <LocalBoard />
     </div>
   )
 }
---
+++ app/javascript/react/components/LocalBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Chessboard from 'chessboardjsx'
+
+const LocalBoard = (props) => {
+  return(
+    <Chessboard position="start" />
+  )
+}
+
+export default LocalBoard
