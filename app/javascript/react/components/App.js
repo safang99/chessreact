4c813bcc52f6137d5ba41069990b0cda868c5a0a
Stephen Fang <safang99@gmail.com>
2019-11-04 13:35:54 -0500

    redirect on channel input

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from "react-router-dom"
 
 import LocalBoard from "./LocalBoard"
 import WelcomeIndex from "./WelcomeIndex"
+import OnlineIndexContainer from "./OnlineIndexContainer"
 import OnlineBoardContainer from "./OnlineBoardContainer"
 
 export const App = (props) => {
@@ -11,6 +12,7 @@ export const App = (props) => {
       <Switch>
         <Route exact path="/" component={WelcomeIndex} />
         <Route exact path="/local" component={LocalBoard} />
+        <Route exact path="/online/index" component={OnlineIndexContainer} />
         <Route exact path="/online/:id" component={OnlineBoardContainer} />
       </Switch>
     </BrowserRouter>
