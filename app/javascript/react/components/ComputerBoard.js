8e285a6ad5adbe62d02eb5f577c2871d2c0b1089
Stephen Fang <safang99@gmail.com>
2019-11-07 18:36:30 -0500

    Slow computer but not dumb

--- app/javascript/react/components/ComputerBoard.js
+++ app/javascript/react/components/ComputerBoard.js
@@ -14,26 +14,260 @@ const ComputerBoard = (props) => {
     pgnArray: []
   })
 
-  const calculateBestMove = (game) => {
+  const minimaxRoot = (depth, game, isMaximisingPlayer) => {
+
     let newGameMoves = game.moves();
-    return newGameMoves[Math.floor(Math.random() * newGameMoves.length)];
+    let bestMove = -9999;
+    let bestMoveFound;
+
+    for(let i = 0; i < newGameMoves.length; i++) {
+        let newGameMove = newGameMoves[i]
+        game.move(newGameMove);
+        let value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+        game.undo();
+        if(value >= bestMove) {
+            bestMove = value;
+            bestMoveFound = newGameMove;
+        }
+    }
+    return bestMoveFound;
   };
+
+  // let currentBoard = () => {
+  //     var output = [],
+  //       row = [];
+  //
+  //     for (var i = SQUARES.a8; i <= SQUARES.h1; i++) {
+  //       if (board[i] == null) {
+  //         row.push(null);
+  //       } else {
+  //         row.push({ type: board[i].type, color: board[i].color });
+  //       }
+  //       if ((i + 1) & 0x88) {
+  //         output.push(row);
+  //         row = [];
+  //         i += 8;
+  //       }
+  //     }
+  //     return output;
+  //   }
+
+  const minimax = (depth, game, alpha, beta, isMaximisingPlayer) => {
+    positionCount++;
+    if (depth === 0) {
+      let squares = game.SQUARES
+      let currentBoard = () => {
+        let output = []
+        let row = []
+
+        for (var i = 0; i < squares.length; i++) {
+          squares[i]
+          if (game.get(squares[i]) === null) {
+            row.push(null)
+          } else {
+            row.push(game.get(squares[i]))
+          }
+          if ((i + 1) % 8 === 0) {
+            output.push(row)
+            row = []
+          }
+        }
+        return output
+      }
+        return -evaluateBoard(currentBoard);
+    }
+
+    let newGameMoves = game.moves();
+
+    if (isMaximisingPlayer) {
+        let bestMove = -9999;
+        for (let i = 0; i < newGameMoves.length; i++) {
+            game.move(newGameMoves[i]);
+            bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+            game.undo();
+            alpha = Math.max(alpha, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove;
+    } else {
+        let bestMove = 9999;
+        for (let i = 0; i < newGameMoves.length; i++) {
+            game.move(newGameMoves[i]);
+            bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+            game.undo();
+            beta = Math.min(beta, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove;
+    }
+  };
+
+  const evaluateBoard = (board) => {
+    let totalEvaluation = 0;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            totalEvaluation = totalEvaluation + getPieceValue(board()[i][j], i ,j);
+        }
+    }
+    return totalEvaluation;
+  };
+
+  const reverseArray = (array) => {
+    return array.slice().reverse();
+  };
+
+  const pawnEvalWhite =
+    [
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+        [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+        [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+        [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+        [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+        [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+        [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+    ];
+
+  const pawnEvalBlack = reverseArray(pawnEvalWhite);
+
+  const knightEval =
+    [
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+        [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+        [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+        [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+        [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+        [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+        [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+    ];
+
+  const bishopEvalWhite = [
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+  ];
+
+  const bishopEvalBlack = reverseArray(bishopEvalWhite);
+
+  const rookEvalWhite = [
+    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+  ];
+
+  const rookEvalBlack = reverseArray(rookEvalWhite);
+
+  const evalQueen =
+    [
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+  ];
+
+  const kingEvalWhite = [
+
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
+    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+  ];
+
+  const kingEvalBlack = reverseArray(kingEvalWhite);
+
+
+
+
+  const getPieceValue = (piece, x, y) => {
+    if (piece === null) {
+        return 0;
+    }
+    let getAbsoluteValue = (piece, isWhite, x ,y) => {
+        if (piece.type === 'p') {
+            return 10 + ( isWhite ? pawnEvalWhite[y][x] : pawnEvalBlack[y][x] );
+        } else if (piece.type === 'r') {
+            return 50 + ( isWhite ? rookEvalWhite[y][x] : rookEvalBlack[y][x] );
+        } else if (piece.type === 'n') {
+            return 30 + knightEval[y][x];
+        } else if (piece.type === 'b') {
+            return 30 + ( isWhite ? bishopEvalWhite[y][x] : bishopEvalBlack[y][x] );
+        } else if (piece.type === 'q') {
+            return 90 + evalQueen[y][x];
+        } else if (piece.type === 'k') {
+            return 900 + ( isWhite ? kingEvalWhite[y][x] : kingEvalBlack[y][x] );
+        }
+        throw "Unknown piece type: " + piece.type;
+    };
+
+    let absoluteValue = getAbsoluteValue(piece, piece.color === 'w', x ,y);
+    return piece.color === 'w' ? absoluteValue : -absoluteValue;
+  };
+
+
+  // const calculateBestMove = (game) => {
+  //   let newGameMoves = game.moves();
+  //   return newGameMoves[Math.floor(Math.random() * newGameMoves.length)];
+  // };
 
   const makeBestMove = () => {
-      let bestMove = getBestMove(game.current);
-      game.current.move(bestMove);
+    let bestMove = getBestMove(game.current);
+    game.current.move(bestMove);
 
-      if (game.current.game_over()) {
-          alert('Game over');
-      }
+    setBoard(
+      {...board,
+        fen: game.current.fen(),
+        history: game.current.history({ verbose: true }),
+        squareStyles: squareStyling({pieceSquare: board.pieceSquare, history: board.history}),
+        pgnArray: game.current.pgn({ max_width: 5, newline_char: ","}).split(",")
+      })
+
+    if (game.current.game_over()) {
+        alert('Game over');
+    }
   };
 
+  // const getBestMove = (game) => {
+  //     if (game.game_over()) {
+  //         alert('Game over');
+  //     }
+  //     let bestMove = calculateBestMove(game);
+  //     return bestMove;
+  // };
+
+  let positionCount
   const getBestMove = (game) => {
-      if (game.game_over()) {
-          alert('Game over');
-      }
-      let bestMove = calculateBestMove(game);
-      return bestMove;
+    if (game.game_over()) {
+        alert('Game over');
+    }
+
+    positionCount = 0;
+    let depth = 3
+    let bestMove = minimaxRoot(depth, game, true);
+
+    return bestMove;
   };
 
   const squareStyling = ({ pieceSquare, history }) => {
@@ -95,7 +329,6 @@ const ComputerBoard = (props) => {
     });
 
     if (move === null) return;
-    console.log(game.current.pgn())
 
     setBoard(
       {...board,
@@ -106,15 +339,7 @@ const ComputerBoard = (props) => {
       })
 
       window.setTimeout(makeBestMove(), 250);
-      console.log(game.current.pgn())
 
-    setBoard(
-      {...board,
-        fen: game.current.fen(),
-        history: game.current.history({ verbose: true }),
-        squareStyles: squareStyling({pieceSquare: board.pieceSquare, history: board.history}),
-        pgnArray: game.current.pgn({ max_width: 5, newline_char: ","}).split(",")
-      })
   };
 
   const onMouseOverSquare = square => {
@@ -166,14 +391,14 @@ const ComputerBoard = (props) => {
     });
 
     window.setTimeout(makeBestMove(), 250);
-
-    setBoard(
-      {...board,
-      fen: game.current.fen(),
-      history: game.current.history({ verbose: true }),
-      pieceSquare: "",
-      pgnArray: game.current.pgn({ max_width: 5, newline_char: ","}).split(",")
-    });
+    //
+    // setBoard(
+    //   {...board,
+    //   fen: game.current.fen(),
+    //   history: game.current.history({ verbose: true }),
+    //   pieceSquare: "",
+    //   pgnArray: game.current.pgn({ max_width: 5, newline_char: ","}).split(",")
+    // });
   };
 
   const pgn = game.current.pgn({ max_width: 5, newline_char: <br></br> })
